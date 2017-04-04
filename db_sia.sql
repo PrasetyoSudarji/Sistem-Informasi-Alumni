782b9431f4b4afa05678050856c14a908a4c9e2e
Prasetyo S <prasetyosudarji@gmail.com>
2017-04-04 19:43:28 +0700

    Update
    database

--- db_sia.sql
+++ db_sia.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.2.11
--- http://www.phpmyadmin.net
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 26, 2017 at 04:43 AM
--- Server version: 5.6.21
--- PHP Version: 5.6.3
+-- Generation Time: Apr 04, 2017 at 02:42 PM
+-- Server version: 10.1.21-MariaDB
+-- PHP Version: 5.6.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,7 +14,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `db_sia`
@@ -23,10 +23,31 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `email` varchar(32) NOT NULL,
+  `username` varchar(32) NOT NULL,
+  `password` varchar(32) NOT NULL,
+  `status` enum('1','2','3') NOT NULL DEFAULT '2'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`email`, `username`, `password`, `status`) VALUES
+('admin2@itera.ac.id', 'admin2', 'admin2', '2'),
+('admin@itera.ac.id', 'admin', 'admin', '1');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `data_alamat`
 --
 
-CREATE TABLE IF NOT EXISTS `data_alamat` (
+CREATE TABLE `data_alamat` (
   `nim` int(8) NOT NULL,
   `rt` varchar(3) DEFAULT NULL,
   `rw` varchar(3) DEFAULT NULL,
@@ -41,7 +62,7 @@ CREATE TABLE IF NOT EXISTS `data_alamat` (
 --
 
 INSERT INTO `data_alamat` (`nim`, `rt`, `rw`, `desa/kelurahan`, `kecamatan`, `kabupaten/kota`, `provinsi`) VALUES
-(14113003, '018', '004', 'karang endah', 'terbanggi besar', 'lampung tengah', 'Lampung');
+(14113003, NULL, NULL, NULL, NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -49,13 +70,48 @@ INSERT INTO `data_alamat` (`nim`, `rt`, `rw`, `desa/kelurahan`, `kecamatan`, `ka
 -- Table structure for table `lowongan`
 --
 
-CREATE TABLE IF NOT EXISTS `lowongan` (
-`id` int(8) NOT NULL,
+CREATE TABLE `lowongan` (
+  `kode` varchar(6) NOT NULL,
   `prodi` varchar(32) DEFAULT NULL,
   `nama_perusahaan` varchar(32) NOT NULL,
   `expired_time` date NOT NULL,
-  `skill` text
+  `deskripsi` text
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `lowongan`
+--
+
+INSERT INTO `lowongan` (`kode`, `prodi`, `nama_perusahaan`, `expired_time`, `deskripsi`) VALUES
+('AR1', 'Teknik Arsitektur', 'Art', '2017-04-12', 'aman'),
+('IF1', 'Teknik Informatika', 'Gameloft', '2017-04-19', 'aman');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `lowongan_counter`
+--
+
+CREATE TABLE `lowongan_counter` (
+  `prodi` varchar(3) NOT NULL,
+  `counter` int(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `lowongan_counter`
+--
+
+INSERT INTO `lowongan_counter` (`prodi`, `counter`) VALUES
+('AR', 2),
+('EL', 1),
+('FI', 1),
+('GD', 1),
+('IF', 2),
+('PWK', 1),
+('SI', 1),
+('TG', 1),
+('TL', 1),
+('UM', 1);
 
 -- --------------------------------------------------------
 
@@ -63,13 +119,15 @@ CREATE TABLE IF NOT EXISTS `lowongan` (
 -- Table structure for table `mahasiswa`
 --
 
-CREATE TABLE IF NOT EXISTS `mahasiswa` (
+CREATE TABLE `mahasiswa` (
+  `email` varchar(32) NOT NULL,
   `nim` int(8) NOT NULL,
   `nama` varchar(32) NOT NULL,
+  `username` varchar(32) NOT NULL,
   `prodi` varchar(32) DEFAULT NULL,
   `tahun_lulus` int(4) DEFAULT NULL,
   `foto` varchar(32) NOT NULL DEFAULT 'default.png',
-  `status` enum('1','2') NOT NULL,
+  `status` enum('1','2','3') NOT NULL DEFAULT '3',
   `jumlah_skill` int(2) DEFAULT NULL,
   `linked_in` varchar(32) DEFAULT NULL,
   `fb` varchar(32) DEFAULT NULL,
@@ -81,9 +139,8 @@ CREATE TABLE IF NOT EXISTS `mahasiswa` (
 -- Dumping data for table `mahasiswa`
 --
 
-INSERT INTO `mahasiswa` (`nim`, `nama`, `prodi`, `tahun_lulus`, `foto`, `status`, `jumlah_skill`, `linked_in`, `fb`, `twitter`, `instagram`) VALUES
-(1, 'admin', NULL, NULL, 'default.png', '1', NULL, NULL, NULL, NULL, NULL),
-(14113003, 'Prasetyo Sudarji', 'Teknik Informatika', 2018, 'default.png', '2', 4, 'prasetyosudarji', 'prasetyosudarji', 'prasetyosudarji', 'prasetyosudarji');
+INSERT INTO `mahasiswa` (`email`, `nim`, `nama`, `username`, `prodi`, `tahun_lulus`, `foto`, `status`, `jumlah_skill`, `linked_in`, `fb`, `twitter`, `instagram`) VALUES
+('prasetyosudarji@gmail.com', 14113003, 'prasetyo sudarji', 'prasetyo', 'Teknik Informatika', 2018, 'default.png', '3', 5, NULL, NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -91,7 +148,7 @@ INSERT INTO `mahasiswa` (`nim`, `nama`, `prodi`, `tahun_lulus`, `foto`, `status`
 -- Table structure for table `prodi`
 --
 
-CREATE TABLE IF NOT EXISTS `prodi` (
+CREATE TABLE `prodi` (
   `id` varchar(3) NOT NULL,
   `nama` varchar(32) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -109,45 +166,56 @@ INSERT INTO `prodi` (`id`, `nama`) VALUES
 ('GD', 'Teknik Geomatika'),
 ('IF', 'Teknik Informatika'),
 ('TL', 'Teknik Lingkungan'),
-('SI', 'Teknik Sipil');
+('SI', 'Teknik Sipil'),
+('UM', 'Umum');
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`email`);
+
+--
 -- Indexes for table `data_alamat`
 --
 ALTER TABLE `data_alamat`
- ADD PRIMARY KEY (`nim`);
+  ADD PRIMARY KEY (`nim`);
 
 --
 -- Indexes for table `lowongan`
 --
 ALTER TABLE `lowongan`
- ADD PRIMARY KEY (`id`), ADD KEY `prodi` (`prodi`), ADD KEY `prodi_2` (`prodi`);
+  ADD PRIMARY KEY (`kode`),
+  ADD UNIQUE KEY `kode` (`kode`),
+  ADD KEY `prodi` (`prodi`),
+  ADD KEY `prodi_2` (`prodi`);
+
+--
+-- Indexes for table `lowongan_counter`
+--
+ALTER TABLE `lowongan_counter`
+  ADD PRIMARY KEY (`prodi`);
 
 --
 -- Indexes for table `mahasiswa`
 --
 ALTER TABLE `mahasiswa`
- ADD PRIMARY KEY (`nim`), ADD KEY `prodi` (`prodi`);
+  ADD PRIMARY KEY (`email`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD UNIQUE KEY `nim` (`nim`),
+  ADD KEY `prodi` (`prodi`);
 
 --
 -- Indexes for table `prodi`
 --
 ALTER TABLE `prodi`
- ADD PRIMARY KEY (`id`), ADD KEY `nama` (`nama`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `nama` (`nama`);
 
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `lowongan`
---
-ALTER TABLE `lowongan`
-MODIFY `id` int(8) NOT NULL AUTO_INCREMENT;
 --
 -- Constraints for dumped tables
 --
@@ -156,19 +224,25 @@ MODIFY `id` int(8) NOT NULL AUTO_INCREMENT;
 -- Constraints for table `data_alamat`
 --
 ALTER TABLE `data_alamat`
-ADD CONSTRAINT `data_alamat_ibfk_1` FOREIGN KEY (`nim`) REFERENCES `mahasiswa` (`nim`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `data_alamat_ibfk_1` FOREIGN KEY (`nim`) REFERENCES `mahasiswa` (`nim`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `lowongan`
 --
 ALTER TABLE `lowongan`
-ADD CONSTRAINT `lowongan_ibfk_1` FOREIGN KEY (`prodi`) REFERENCES `prodi` (`nama`) ON DELETE CASCADE ON UPDATE NO ACTION;
+  ADD CONSTRAINT `lowongan_ibfk_1` FOREIGN KEY (`prodi`) REFERENCES `prodi` (`nama`) ON DELETE CASCADE ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `lowongan_counter`
+--
+ALTER TABLE `lowongan_counter`
+  ADD CONSTRAINT `lowongan_counter_ibfk_1` FOREIGN KEY (`prodi`) REFERENCES `prodi` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `mahasiswa`
 --
 ALTER TABLE `mahasiswa`
-ADD CONSTRAINT `mahasiswa_ibfk_1` FOREIGN KEY (`prodi`) REFERENCES `prodi` (`nama`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `mahasiswa_ibfk_1` FOREIGN KEY (`prodi`) REFERENCES `prodi` (`nama`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
